c870a8b9ffeec7f776fdbd9619560ea8d3a97ea4
hungnv <vanhungbkcbg1@gmail.com>
2020-04-08 16:49:37 +0700

    php-nginx

--- code/index.php
+++ code/index.php
@@ -2,5 +2,14 @@
 
 //get system variable
 $api_url=$_ENV["api_url"]?? "not set"; // -> return test(declared in docker-compose file )
+$cURLConnection = curl_init();
+
+curl_setopt($cURLConnection, CURLOPT_URL, $api_url);
+curl_setopt($cURLConnection, CURLOPT_RETURNTRANSFER, true);
+
+$result = curl_exec($cURLConnection);
+curl_close($cURLConnection);
+
 echo $api_url;
+echo $result;
 //phpinfo();
